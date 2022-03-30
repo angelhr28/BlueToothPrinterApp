595e6b9aff1aac03cf85b2c91e06c43ff9437565
Newton Angel Huamanñahui Robles <newton.huamannahui@promart.pe>
2022-03-29 21:41:10 -0500

    Permisos para conexion a bluetooth

--- app/src/main/java/com/example/myprinterbluetooth/utils/BluetoothDriver.kt
+++ app/src/main/java/com/example/myprinterbluetooth/utils/BluetoothDriver.kt
@@ -6,11 +6,9 @@ import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
 import android.content.Context
-import android.util.Log
 import java.io.InputStream
 import java.io.OutputStream
 import java.util.*
-
 
 class BluetoothDriver(private val context: Context) {
     // Para la operaciones con dispositivos bluetooth
@@ -37,26 +35,17 @@ class BluetoothDriver(private val context: Context) {
         // Obtenemos el dispositivo con la direccion seleccionada en la lista
         bluetoothDevice = bluetoothAdapter.getRemoteDevice(address)
         try {
-            Log.e("TAG", "1")
             // Conectamos los dispositivos
             bluetoothSocket =
                 bluetoothDevice?.createRfcommSocketToServiceRecord(
                     aplicacionUUID
                 )
-            Log.e("TAG", "2  $bluetoothSocket")
             bluetoothSocket?.connect() // conectamos el socket
-            Log.e("TAG", "3")
             outputStream = bluetoothSocket?.outputStream
             inputStream = bluetoothSocket?.inputStream
-            Log.e("TAG", "FIN")
 
-            //empezarEscucharDatos();
             listener("Dispositivo Conectado $name")
         } catch (e: Exception) {
-            Log.e(
-                "ERROR",
-                "Error al conectar el dispositivo bluetooth ---  ${e.message}"
-            )
             listener("No se pudo conectar el dispositivo $name")
         }
     }
